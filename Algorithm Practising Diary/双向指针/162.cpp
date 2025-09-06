fc2733b0316f697afb1421c8697f0c92bd3a6b70
HU SHENGQUAN <h127429916@gmail.com>
2025-09-06 11:24:47 +0800

    Update 162.cpp

--- Algorithm Practising Diary/双向指针/162.cpp
+++ Algorithm Practising Diary/双向指针/162.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 using namespace std;
 
+//本题是二分查找一节的内容，虽然可能有多个峰顶，但是题目仅仅要求寻找一个
 int main()
 {
     int num[1024],n;
@@ -11,16 +12,25 @@ int main()
         cin>>num[i];
     }
     int left=0,right=n-1;
+    //构建数组，定义指针
+    //闭区间法
     while(left<=right)
     {
         int mid=(left+right)/2;
-        if(num[mid]>num[mid+1])
+        /*定义观察方向为数组索引递增方向，若num[mid]>num[mid]+1，即下坡，则可以认为
+        mid之后都是下坡路段*/
+        if(num[mid]>=num[mid+1])
         {
+            //红蓝染色法，下坡路段（包含可能的峰顶）染蓝色。
+            //论域调整，跳过染色区域
             right=mid-1;
         }else{
+            //反之染红色，跳过染色区域
             left=mid+1;
         }
     }
+    //闭区间二分法left就是答案
     cout<<left;
     return 0;
+
 }
