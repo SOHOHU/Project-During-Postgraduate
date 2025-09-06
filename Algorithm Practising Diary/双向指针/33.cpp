5c3fbddf969d5ab551cccdab18bf11b9fe6e88f7
HU SHENGQUAN <h127429916@gmail.com>
2025-09-06 22:19:10 +0800

    Update 33.cpp

--- Algorithm Practising Diary/双向指针/33.cpp
+++ Algorithm Practising Diary/双向指针/33.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 using namespace std;
 
+//前言：循环数组可以使用二分查找，本质上是红蓝染色法，这题用灵神的坐标图秒了
 int main()
 {
     int num[1024],n,target;
@@ -11,26 +12,30 @@ int main()
         cin>>num[i];
     }
     int left=0,right=n-1;
-
     while(left<=right)
     {
         int mid=(left+right)/2;
-        if(num[mid]>num[n-1])
+        //如果mid大于right，那么旋转次数必然大于n/2，且mid两边保持升序
+        if(num[mid]>num[right])
         {
             if(target>num[mid])
             {
+                //如果target还大于num[mid]，那么target只可能在mid和right之间（准确说是mid和最大值的索引之间，但是此时需要用二分法继续）
                 left=mid+1;
-            }else if(target<num[mid]&&target>num[n-1])
+            }else if(target<num[mid]&&target>num[right])
             {
+                //此时必然在left到mid之间
                 right=mid-1;
             }else{
+                //其他情况在mid和right之间
                 left=mid+1;
             }
         }else{
+            // 同理，只需要直到Target所在的区间二分即可
             if(target<num[mid])
             {
                 right=mid-1;
-            }else if(target>num[mid]&&target<num[n-1])
+            }else if(target>num[mid]&&target<num[right])
             {
                 left=mid+1;
             }else{
@@ -45,4 +50,5 @@ int main()
     }
     cout<<left;
     return 0;
+
 }
