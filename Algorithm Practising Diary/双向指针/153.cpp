f3ac4418282c299c55013a0ff3a64e6ab08e208b
HU SHENGQUAN <h127429916@gmail.com>
2025-09-06 10:05:41 +0800

    Update 153.cpp

--- Algorithm Practising Diary/双向指针/153.cpp
+++ Algorithm Practising Diary/双向指针/153.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    //构建数组
     int num[1024],n;
     cin>>n;
     for(int i=0;i<n;i++)
@@ -11,9 +12,12 @@ int main()
         cin>>num[i];
     }
     int left=0,right=n-1;
+    //周期序列也可以使用二分查找
     while(left<=right)
     {
+        
         int mid=(left+right)/2;
+        
         if(num[mid]>num[n-1])
         {
             left=mid+1;
@@ -23,4 +27,5 @@ int main()
     }
     cout<<left;
     return 0;
+
 }
