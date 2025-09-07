3ebba2df09a502997f01a4c831d8e78996ca031c
HU SHENGQUAN <h127429916@gmail.com>
2025-09-07 20:40:33 +0800

    Update 2958.cpp

--- Algorithm Practising Diary/滑动窗口/2958.cpp
+++ Algorithm Practising Diary/滑动窗口/2958.cpp
@@ -14,15 +14,19 @@ int main()
     unordered_map<int,int>map;
     map[num[left]]++;
     int ans=0,len=1;
+
+    //初始化
     for(int right=1;right<n;right++)
     {
         map[num[right]]++;
+        //每次加入最右侧的值，必然是最右侧先相应是否重复次数大于k
         if(map[num[right]]>k)
         {
             len--;
         }
         len++;
         ans=max(ans,len);
+        //左侧收缩，扣去对应的hash，直到得到符合条件的数组
         while(map[num[right]]>k)
         {
             map[num[left]]--;
@@ -37,4 +41,5 @@ int main()
     }
     return 0;
 }
+
 
