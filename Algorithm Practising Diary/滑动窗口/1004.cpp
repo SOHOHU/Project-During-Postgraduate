1bdf2315e1d5a3cd72f53b5111129f40afa87324
HU SHENGQUAN <h127429916@gmail.com>
2025-09-07 20:21:20 +0800

    Update 1004.cpp

--- Algorithm Practising Diary/滑动窗口/1004.cpp
+++ Algorithm Practising Diary/滑动窗口/1004.cpp
@@ -11,17 +11,19 @@ int main()
         cin>>num[i];
     }
     int left=0,ans=0,len=1;
+    //输入数组
     unordered_map<int,int>map;
     map[num[left]]++;
     for(int right=1;right<n;right++)
     {
+        //滑动窗口向右扩张
         map[num[right]]++;
         len++;
         if(map[0]<=k)
         {
             ans=max(ans,len);
         }
-
+        //可以容忍K个0
         while(map[0]>k)
         {
             map[num[left]]--;
@@ -31,4 +33,5 @@ int main()
     }
     cout<<ans;
     return 0;
+
 }
