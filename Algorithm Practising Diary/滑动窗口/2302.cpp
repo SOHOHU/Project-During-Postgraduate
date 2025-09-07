182203f622bfb872bd79385c978095791a50e847
HU SHENGQUAN <h127429916@gmail.com>
2025-09-07 20:34:56 +0800

    Update 2302.cpp

--- Algorithm Practising Diary/滑动窗口/2302.cpp
+++ Algorithm Practising Diary/滑动窗口/2302.cpp
@@ -12,10 +12,11 @@ int main()
         cin>>num[i];
     }
     int left=0,ans=0,score=num[left],sum=0;
+    
     if(num[left]<k){
         ans++;
     }
-
+    //初始化，开始判断第一个（因为right是开区间）
     for(int right=1;right<n;right++)
     {
         for(int i=left;i<=right;i++)
@@ -23,6 +24,7 @@ int main()
             sum+=num[i];
         }
         score=sum*(right-left+1);
+        //超过重新算分，左侧收缩
         while(score>=k)
         {
             left++;
@@ -34,9 +36,11 @@ int main()
             score=sum*(right-left+1);
         }
         int len=right-left+1;
+        //这里审题错了，让求的是数目不是最长序列长，不过原理是一样的
         ans+=len;
         sum=0;
     }
     cout<<ans;
     return 0;
+
 }
