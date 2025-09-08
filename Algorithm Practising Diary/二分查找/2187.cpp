b4b3ed748ecbc7ee932310aeaacc805c27abe64e
HU SHENGQUAN <h127429916@gmail.com>
2025-09-08 20:21:08 +0800

    Update 2187.cpp

--- Algorithm Practising Diary/二分查找/2187.cpp
+++ Algorithm Practising Diary/二分查找/2187.cpp
@@ -12,18 +12,22 @@ int main()
     }
     sort(num,num+n);
     int max_time=0;
+    // 初始化 x为二分猜答案的论域
     for(int j=1;j<=num[n-1];j++)
     {
         x[j]=j;
     }
+    // 进行二分查找
     int left=1,right=num[n-1],sum=0,t;
     while(left<=right)
     {
         int mid=(left+right)/2;
+        //计算此时的时间
         for(int i=0;i<n;i++)
         {
             sum+=x[mid]/num[i];
         }
+        // 红蓝染色法，如果小，则所有x往左的值全部不行，反之右侧全部不行，单调性
         if(sum<totaltrip)
         {
             left=mid+1;
@@ -44,5 +48,6 @@ int main()
     }
     cout << x[left];
     return 0;
+
 
 }
